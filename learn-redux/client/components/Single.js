2bc8002c3dd083f24af1c9f442704f9628cbecc8
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-01-20 03:26:13 -0600

    finished course

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Photo from './Photo';
-// import comments
+import Comments from './Comments';
 
 class Single extends Component {
   render() {
-    // index of the post
-    const i = this.props.posts.findIndex(
-      (post) => post.code === this.props.params.postId
-    );
+    const { postId } = this.props.params;
+
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[i];
-    // get us the post
+
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-        <Photo i={i} post={post} {...this.props} />;
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     );
   }
--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -10,12 +10,23 @@ import posts from './data/posts';
 
 // create an object from default data
 const defaultState = {
-    posts,
-    comments
-}
+  posts,
+  comments
+};
 
-const store = createStore(rootReducer, defaultState);
+const store = createStore(
+  rootReducer,
+  defaultState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
